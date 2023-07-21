9cc20c71b5c983ec7a7cbaf8d5d111f82eaa8274
Felipe F Araujo <devfelipefaraujo@gmail.com>
2023-07-21 09:41:13 -0300

    Página 1 - Iniciante

--- src/pagina1/bee1002.java
+++ src/pagina1/bee1002.java
@@ -1,2 +1,16 @@
-package pagina1;public class bee1002 {
+package pagina1;
+
+import java.util.Scanner;
+
+public class bee1002 {
+    public static void main(String[] args) {
+        double n = 3.14159;
+
+        Scanner input = new Scanner(System.in);
+        double raio = input.nextDouble();
+
+        double area = n * raio * raio;
+
+        System.out.printf("A=%.4f\n", area);
+    }
 }
--- src/pagina1/bee1003.java
+++ src/pagina1/bee1003.java
@@ -2,15 +2,12 @@ package pagina1;
 
 import java.util.Scanner;
 
-public class bee1001 {
+public class bee1003 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         int a = input.nextInt();
         int b = input.nextInt();
-
-        int x = a + b;
-
-        System.out.println("X = " + x);
+        int soma = a + b;
+        System.out.printf("SOMA = %d\n", soma);
     }
 }
