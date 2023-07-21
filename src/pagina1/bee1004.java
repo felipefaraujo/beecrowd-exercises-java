124b5eb8923f0fd16dce184158bbeb4ebf412a2e
Felipe F Araujo <devfelipefaraujo@gmail.com>
2023-07-21 17:35:34 -0300

    Página 1 - Iniciante

---
+++ src/pagina1/bee1004.java
@@ -0,0 +1,13 @@
+package pagina1;
+
+import java.util.Scanner;
+
+public class bee1004 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        double prod = a * b;
+        System.out.printf("PROD = ", prod);
+    }
+}
